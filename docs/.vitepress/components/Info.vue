e9d98c32606b877b2f6f23f9503827cef0ad8ee9
Hacker-C <1507559148@qq.com>
2022-08-22 23:09:31 +0800

    🐛 fix: 修复链接失效问题

--- docs/.vitepress/components/Info.vue
+++ docs/.vitepress/components/Info.vue
@@ -13,34 +13,28 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Info',
-  data() {
-    return {
-      list: [
-        {
-          text: '奇技淫巧',
-          link: '/JS/uncanny-tricks/ch01',
-          desc: 'JS 一些诡异的行为的解释和花里胡哨的操作，有些看着没啥用，实则蕴含了 JS 底层的一些原理。'
-        },
-        {
-          text: '查漏补缺',
-          link: '/JS/check-for-gaps/ch01',
-          desc: 'JS中容易遗忘或者遗漏的知识点和难点，基于本人自身而言的，更多的是一些笔记。'
-        },
-        {
-          text: '代码模板',
-          link: '/JS/code-templates/ch01',
-          desc: '常用的一些代码模板，包括但不限于常规算法题中的模板等等。'
-        }
-      ]
-    }
+<script setup>
+import { useRouter } from 'vitepress'
+const router = useRouter()
+
+const list = [
+  {
+    text: '奇技淫巧',
+    link: '/JS/uncanny-tricks/ch01',
+    desc: 'JS 一些诡异的行为的解释和花里胡哨的操作，有些看着没啥用，实则蕴含了 JS 底层的一些原理。'
   },
-  methods: {
-    goto(link) {
-      this.$router.push(link)
-    }
+  {
+    text: '查漏补缺',
+    link: '/JS/check-for-gaps/ch01',
+    desc: 'JS中容易遗忘或者遗漏的知识点和难点，基于本人自身而言的，更多的是一些笔记。'
+  },
+  {
+    text: '代码模板',
+    link: '/JS/code-templates/ch01',
+    desc: '常用的一些代码模板，包括但不限于常规算法题中的模板等等。'
   }
+]
+const goto = link => {
+  router.go(link)
 }
 </script>
--- docs/JS/intro.md
+++ docs/JS/intro.md
@@ -1,5 +1,10 @@
 # 我不知道的 JS
 
+## 最近更新
+
+- 查漏补缺：[12-Symbol 内置属性的妙用](/JS/check-for-gaps/ch12)
+- 查漏补缺：[11-深入类型判断](/JS/check-for-gaps/ch11)
+
 ## 目录说明
 
 <Info />
